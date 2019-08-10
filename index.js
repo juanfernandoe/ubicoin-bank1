1ccf8334ef217340d5f41e0ab95d5c69b9a18467
Juan Fernando Estrada <fernandoe@supercodde.com>
2019-08-10 18:51:14 +1000

    simple dynamic form.

--- index.js
+++ index.js
@@ -1,46 +1,74 @@
+'use strict';
+
 new Vue({
 	el: '#app',
 	data: {
 		message: 'Hello Vue.js!',
 		name: '',
+
 		form1: {
-			name: ''
-		},
-		form2: {
-			name: ''
+			meta: [],
+			fields: [],
+			record: {}
 		}
+
 	},
 
 	mounted: function () {
 		console.log("hello mounted");
-		axios.get('meta').then(function(res) {
-			this.meta = parse_rec(res.data)[0];
-			console.log(this.meta);
-		})
-			.catch(function(ex){
-				console.error(ex); 
+
+		axios.get('https://ubi.aiki.info/app/meta')
+			.then((res) => {
+				this.form1.meta = parse_rec(res.data)[0];
+				console.log(JSON.stringify(this.form1.meta));
+			})
+			.catch((ex) => {
+				console.error(ex);
 			});
+	},
+
+	methods: {
+		hello: function () {
+			alert('hello ' + this.form1.name);
 		},
 
-		methods: {
-			hello: function () {
-				alert('hello ' + this.form1.name);
-			},
+		fetch: function () {
 
-			fetch: function () {
-
-			console.log('fetch');
-
-			var url = '/1/a';
+			var url = 'https://ubi.aiki.info/1/a';
 			axios.get(url)
-				.then(function(res){
-			var [recs, fields] = parse_rec(res.data);
-					console.log(fields, recs);
+				.then((res) => {
+					var [record, fields] = parse_rec(res.data);
+					this.form1.fields = fields;
+					this.form1.record = record;
+
+					this.$forceUpdate();
+
+
+					console.log(fields, record);
 				})
-				.catch(function(ex){
-				   console.error(ex); 
+				.catch((ex) => {
+					console.error(ex);
 				});
+		},
+
+		formStructure: function () {
+			let st = [];
+
+			var fields = this.form1.fields;
+			var meta = this.form1.meta[0];
+
+			for (var i = 0; i < fields.length; i++) {
+				var f_name = fields[i];
+				var f_type = meta[f_name];
+			
+				st.push({
+					name: f_name,
+					type: f_type
+				});
+			}
+			return st;
 		}
+
 	}
 });
 
